c72b5baa6f924534a40fccf81b18ba234a67c30d
felixzhu1989 <zhu-hongfeng@hotmail.com>
2022-07-15 17:14:43 +0800

    dd

--- PrismWpf.WeMail/obj/Debug/net6.0-windows/Views/UserLoginView.g.i.cs
+++ PrismWpf.WeMail/obj/Debug/net6.0-windows/Views/UserLoginView.g.i.cs
@@ -53,7 +53,7 @@ namespace PrismWpf.WeMail.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PrismWpf.WeMail;component/views/userloginview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PrismWpf.WeMail;V1.0.0.0;component/views/userloginview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\UserLoginView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
